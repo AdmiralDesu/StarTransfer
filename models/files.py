073f60e613e827dda883073b4b76190b398a7d08
AdmiralDesu <skyrim.men024@gmail.com>
2023-01-28 16:49:46 +0300

    Исправление модели Files

--- models/files.py
+++ models/files.py
@@ -1,7 +1,7 @@
 import datetime
 from uuid import uuid4
 
-from sqlmodel import SQLModel, Field, UniqueConstraint, MetaData
+from sqlmodel import SQLModel, Field, UniqueConstraint, MetaData, Column, BigInteger
 from pydantic import UUID4
 
 
@@ -12,11 +12,11 @@ class BaseFiles(SQLModel):
     content_type: str = Field(nullable=False)
     created_at = Field(default=datetime.datetime.today())
     comment: str = Field(nullable=True, default="")
-    file_size: int = Field(nullable=False)
+    file_size: int = Field(nullable=False, sa_column=Column(BigInteger()))
 
 
 class Files(BaseFiles, table=True):
-    id: int = Field(primary_key=True, index=True, nullable=False)
+    id: int = Field(primary_key=True, index=True, nullable=False, sa_column=Column(BigInteger(), primary_key=True))
     metadata = MetaData(schema="app")
 
     __tablename__ = "files"
