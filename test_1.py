040d42c13dc72082c2b81cd44aa12993c2f234e7
AdmiralDesu <skyrim.men024@gmail.com>
2023-03-17 17:33:42 +0300

    Updated dependencies

--- test_1.py
+++ test_1.py
@@ -1,44 +1,63 @@
-import asyncio
+import pathlib
 
-import aiohttp
-from aiohttp import FormData
+from config import config
+import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import os
+import magic
+
+magic_type = magic.Magic(mime=True)
 
 
-async def upload_file(
-        url: str,
-        filepath: str
+def upload_file(
+        file_path: pathlib.Path,
+        folder_id: int
 ):
-    data = FormData(quote_fields=True)
-    data.add_field(
-        'file',
-        open(filepath, 'rb')
-    )
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, data=data) as response:
-            print(await response.text())
-            print(response.status)
-
-
-def main():
-    tasks = []
-
-    for i in range(0, 10):
-        tasks.append(
-            upload_file(
-                url="url",
-                filepath="filepath",
-            )
+    mime_type = magic_type.from_file(str(file_path))
+    print(file_path.parts[-1])
+    print(mime_type)
+    files = {
+        "file": (
+            file_path.parts[-1],
+            open(f"{file_path}", "rb"),
+            mime_type
         )
-
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    result = loop.run_until_complete(asyncio.gather(*tasks))
-    print(result)
-    loop.close()
-
-main()
-
-
+    }
+    resp = requests.post(
+        url=f"http://localhost:3561/file/upload_file_to_folder?folder_id={folder_id}",
+        files=files
+    )
+    if resp.ok:
+        return file_path.parts[-1]
+    return Exception(f"Ошибка при закачке файла\n{resp.text}")
 
 
+def main(
+        dir_path: str,
+        folder_id: int
+):
+    file_pathes = [
+        pathlib.Path(os.path.join(dir_path, file)) for file in
+        os.listdir(dir_path)
+        if os.path.isfile(os.path.join(dir_path, file))
+    ]
 
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        tasks = {
+            executor.submit(upload_file, file_path, folder_id): file_path for file_path in file_pathes
+        }
+        for future in as_completed(tasks):
+            try:
+                result = future.result()
+                print(result)
+            except Exception as error:
+                print(error)
+            # if isinstance(result, Exception):
+            #     raise result
+
+
+if __name__ == '__main__':
+    main(
+        dir_path=r"E:\Downloads\Homework 3D\QOC\FF7\Tifa",
+        folder_id=8
+    )
--- test_2.py
+++
@@ -1,8 +0,0 @@
-
-
-
-
-for a in range(1, 10000):
-    for b in range(1, 10000):
-        if a ** 2 * b == 2000 * a + b:
-            print(a, b)
